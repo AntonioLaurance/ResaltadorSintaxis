d5513ed4be08cdd7f4ba47cca6ccd0bbc00d3f79
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2023-03-16 11:11:23 -0600

    Archivo fuente del resaltador

---
+++ resaltador.rkt
@@ -0,0 +1,13 @@
+#lang slideshow
+; Resaltador de sintaxis para proyecto
+
+; Integrantes del equipo:
+;     Ricardo Campos Luna            A01656898
+;     Samuel Sandoval Delgado        A01656191
+;     Luis Roberto Martínez Ramírez  A01662619
+
+; Pedimos archivo del código al usuario
+(display "Nombre de archivo (con extensión): ")
+(define file (read))
+
+
